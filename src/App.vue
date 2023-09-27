f51a68c33bfeb71bf9570e7f011f77b0e338b279
Josue <c.josue.balsac@gmail.com>
2023-09-27 15:53:27 -0500

    2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-  import Navbar from './components/Navbar/Navbar.vue'
+  import Navbar from './components/Navbar/Navbar.vue';
+  import Planet from './components/planet/planet.vue'
 </script>
 
 <template>
   <Navbar/>
+  <Planet/>
 </template>
 
 
---
+++ src/components/planet/planet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grid grid-cols-3 gap-6 px-12">
+        <section class="cal-span-2 flex justify-center">
+            <img src="@/assets/planets/earth.svg" alt="Planeta">
+        </section>
+        <section>
+            <h1></h1>
+            <p></p>
+            <label for="">
+                <span>Source: </span>
+                <a href="#">wikipedia</a>
+                <img src="@/assets/icons/arrow-top-right-on-square.svg" alt="">
+            </label>
+        </section>
+    </div>
+</template>
